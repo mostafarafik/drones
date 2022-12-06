c0e19888ee127077830e1b039133a7180426e4a7
Mostafa_rafik <mustafa.rafiq@sandata.com>
2022-12-06 02:50:02 +0200

    project init

---
+++ src/test/java/com/redcode/drons/DronesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redcode.drons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
